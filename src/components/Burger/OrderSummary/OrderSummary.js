8533948b5cdbd028b966423ed5689c01a827c762
Veneta Stefanova <venetastefanovaa@gmail.com>
2018-07-18 14:46:40 +0300

    improving performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from "../../../hoc/Aux";
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey=>{ // igKey = salad, meat...
-        return  (
-            <li key={igKey}>
-                <span style={{textTransform : 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
-            </li>
-        );
+class OrderSummary extends Component {
+    //this could be a functional component, doesn't have to be a class
+    // componentWillUpdate(){
+    //     console.log("order summary will update");
+    // }
 
-            
-   });
-    return(
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey=>{ // igKey = salad, meat...
+            return  (
+                <li key={igKey}>
+                    <span style={{textTransform : 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
+                </li>
+            );
+    
+                
+       });
+        return(
             <Aux>
                 <h3>Your order</h3>
                 <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.finalPrice.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.finalPrice.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button 
                 btnType="Danger"
-                clicked={props.purchaseCancelled}>CANCEL</Button>
+                clicked={this.props.purchaseCancelled}>CANCEL</Button>
             <Button
                 btnType="Success"
-                clicked={props.purchaseContinued}>CONTINUE</Button>
+                clicked={this.props.purchaseContinued}>CONTINUE</Button>
             </Aux>
-    );
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from "./Modal.css";
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) =>(
-        <Aux>
-            <Backdrop clicked={props.modalClosed} show = {props.show}/>
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+         return (nextProps.show !== this.props.show);
+    }
+
+    componentWillUpdate(){
+        console.log("modal will update");
+    }
+    render(){
+        return(
+            <Aux>
+            <Backdrop clicked={this.props.modalClosed} show = {this.props.show}/>
             <div 
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity : props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity : this.props.show ? '1' : '0'
             }}
             className={classes.Modal}>
-            {props.children}
+            {this.props.children}
         </div>
 
         </Aux>
-    );
+        );
+    }
+}
 
-export default modal;
+export default Modal;
