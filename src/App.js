59eebb9cfba6dcc4e55fcc2870a80549b71b7cc7
Veneta Stefanova <venetastefanovaa@gmail.com>
2019-02-03 16:49:20 +0200

    optimization with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import { Route, withRouter,Switch, Redirect } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
+
+const asyncCheckout = asyncComponent(()=>{
+  return import('./containers/Checkout/Checkout');
+});
+const asyncOrders = asyncComponent(()=>{
+  return import('./containers/Orders/Orders');
+});
+const asyncAuth = asyncComponent(()=>{
+  return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
 
@@ -19,7 +27,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path="/" exact component={BurgerBuilder}/>
-        <Route path="/auth"  component={Auth}/>
+        <Route path="/auth"  component={asyncAuth}/>
         <Redirect to="/"/> 
       </Switch>
     )
@@ -27,10 +35,10 @@ class App extends Component {
     if(this.props.isAuthenticated){
      routes = (
         <Switch>
-          <Route path="/orders" component={Orders}/>
+          <Route path="/orders" component={asyncOrders}/>
           <Route path="/" exact component={BurgerBuilder}/>
-          <Route path="/checkout" component={Checkout}/>        
-          <Route path="/auth"  component={Auth}/>
+          <Route path="/checkout" component={asyncCheckout}/>        
+          <Route path="/auth"  component={asyncAuth}/>
           <Route path="/logout" component={Logout}/>
           <Redirect to="/"/>
 
